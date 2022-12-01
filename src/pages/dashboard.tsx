e851d682f2351a48530e0044c95edb35698f1f88
tkann <t1999-wot@web.de>
2022-12-01 14:01:47 +0100

    Dashboard Layout add Sidebar

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@components/Layouts/Dashboard";
+
+export default function Dashboard(){
+    return(
+        <DashboardLayout>
+            <div className="min-h-screen flex justify-center items-center">
+                Das ist ein Dashboard
+            </div>
+        </DashboardLayout>
+    )
+}
